6d086fedc95615a20cd7fa7892aa02beca8c685c
Slurpee <46894190+radnySlurpee@users.noreply.github.com>
2019-07-11 19:13:01 +0800

    added EEPROM to save locally in the machine

--- arduino-mega-test.ino
+++ arduino-mega-test.ino
@@ -26,9 +26,11 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 String lcdRow1="", lcdRow2="";
 String entryCode = "";
-int slot=-1;
+String userMode = "";
 int arrayPrice[80];
-
+int arrayQuantity[80];
+int address1 = sizeof(arrayPrice);
+bool defaultSetup = false;
 
 void setup()
 { // initialize Serial for testing
@@ -39,28 +41,39 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(interruptPinBills), CurrencyAcceptor, RISING);
   detachInterrupt(digitalPinToInterrupt(interruptPinCoins));
   detachInterrupt(digitalPinToInterrupt(interruptPinBills));
+
+  totalBalance = EEPROM.read(0);
+  EEPROM.write(0,0); 
+  EEPROM.get(1,arrayPrice);
+  EEPROM.put(1, arrayPrice);
+  EEPROM.get(address1,arrayQuantity);
+  EEPROM.put(address1, arrayQuantity);
+if (defaultSetup == false)
+{
+  ItemDataEntry("11", "11", 0, 10, 11);
+  ItemDataEntry("22", "22", 1, 20, 2);
+  ItemDataEntry("33", "33", 2, 30, 3);
+  ItemDataEntry("44", "44", 3, 40, 4);
+  ItemDataEntry("55", "55", 4, 50, 5);
+  ItemDataEntry("66", "66", 5, 60, 6);
+  ItemDataEntry("77", "77", 6, 70, 7);
+  ItemDataEntry("88", "88", 7, 80, 8);
+  ItemDataEntry("99", "99", 8, 90, 9);
+  ItemDataEntry("AA", "AA", 9, 100, 10);
+  ItemDataEntry("BB", "BB", 10, 110, 11);
+  defaultSetup = true;
+}
+
   lcd.begin(20, 4);
   lcd.init();
   lcd.backlight();
-  totalBalance = EEPROM.read(0);
-  for (int ctr = 0; ctr <= 80; ctr++)
-  {
-    arrayPrice[ctr] = EEPROM.read(ctr+1);
-    EEPROM.write(ctr, 0);
-  }
-
-  for (int ctr = 0; ctr <= 81; ctr++)
-  {
-    EEPROM.write(ctr, 0);
-  }
-
   pwm.begin();
   pwm.setPWMFreq(60);
   delay(2000);
   Serial.println("SYSTEM ONLINE");
 }
 
-void LCD_display_customer(){
+void LCD_display(){
   if (totalBalance <= 0){
     lcdRow1 = "Please insert a";
     lcdRow2 = "coin to continue";
@@ -121,19 +134,27 @@ void Keypad_control(){
       {
         entryCode += key;
       }
+      Serial.println(entryCode);
     }else{
       key = key;
     }
 }
 
-void control_function(){
+void control_function_customer(){
   switch (key)
   {
-    case '1': purchase_function(15);
-              servoSpin();
+    case '1': EEPROM.get(1, arrayPrice);
+              purchase_function(arrayPrice[0]);
+              EEPROM.get(address1, arrayQuantity);
+              arrayQuantity[0] = arrayQuantity[0] - 1;
+              EEPROM.put(address1, arrayQuantity);
+              EEPROM.get(address1, arrayQuantity);
+              Serial.println((String)arrayPrice[0]);
+              Serial.println((String)arrayQuantity[0]);
+              servoSpin(0);
               break;
     case '2': purchase_function(25);
-              //servoSpin(1);
+              servoSpin(1);
               break;
     case '3': totalBalance++;
               EEPROM.write(0, totalBalance);
@@ -145,20 +166,72 @@ void control_function(){
   key="";
 }
 
+void control_function_admin(){
+
+  if (entryCode != "")
+  {
+    ItemDataEntry(entryCode, "11", 0, 10, 1);
+    ItemDataEntry(entryCode, "22", 1, 20, 2);
+    ItemDataEntry(entryCode, "33", 2, 30, 3);
+    ItemDataEntry(entryCode, "44", 3, 40, 4);
+    ItemDataEntry(entryCode, "55", 4, 50, 5);
+    ItemDataEntry(entryCode, "66", 5, 60, 6);
+    ItemDataEntry(entryCode, "77", 6, 70, 7);
+    ItemDataEntry(entryCode, "88", 7, 80, 8);
+    ItemDataEntry(entryCode, "99", 8, 90, 9);
+    ItemDataEntry(entryCode, "AA", 9, 100, 10);
+    ItemDataEntry(entryCode, "BB", 10, 110, 11);
+    }
+    if(key == '#'){
+      entryCode = "";
+      key="";
+      lcd.clear();
+    }
+    
+    
+}
+void ItemDataEntry(String thisEntry, String staticEntry, int arrayIndex, int price, int qty)
+{
+  bool itemValid = false;
+  String lcdRow1_admin = "", lcdRow2_admin = "";
+  if (thisEntry == staticEntry)
+  {
+    arrayPrice[arrayIndex] = price;
+    EEPROM.put(1, arrayPrice[arrayIndex]);
+    arrayQuantity[arrayIndex] = qty;
+    EEPROM.put(address1, arrayQuantity[arrayIndex]);
+    lcd.clear();
+    lcdRow1_admin = "CODE: " + (String)staticEntry;
+    lcdRow2_admin = "PHP:" + (String)EEPROM.get(1, arrayPrice[arrayIndex]) + ".00 " +
+                    "QTY:" + (String)EEPROM.get(address1, arrayQuantity[arrayIndex]);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(lcdRow1_admin);
+    lcd.setCursor(0, 1);
+    lcd.print(lcdRow2_admin);
+    Serial.println(lcdRow2_admin);
+    entryCode = "";
+      itemValid = true;
+    }
+    
+    
+}
+
 void purchase_function(int cost){
   int totalBalanceTemp = totalBalance;
   totalBalanceTemp -= cost;
   if (totalBalanceTemp >= 0){
-    totalBalance -= cost;
-    //LCD_display(1, 1, "cost: " + (String)cost);
+    totalBalance = EEPROM.read(0);
+    totalBalance = totalBalance - cost;
+    Serial.println("COST: "+cost);
+    EEPROM.write(0, totalBalance);
   }else{
     totalBalance = totalBalance;
     //LCD_display(1, 1, "Not Enough BAL");
   }
 }
 
-void servoSpin(){
-  slot++;
+void servoSpin(int slot){
    for (int degree = 0; degree <= 545; degree++) //adjust rotation here
    {
      pwm.setPWM(slot, 0, map(90, 0, 180, SERVOMIN, SERVOMAX));
@@ -166,6 +239,7 @@ void servoSpin(){
    }
   pwm.setPWM(slot, 0, 0); //stop
 }
+
 
 void RunCodeInMillis()
 {
@@ -186,7 +260,7 @@ void RunCodeInMillis()
       CurrencyChecker(996, 1001, 1000, "bill");
       deciSeconds1 = 0;
     }
-    if (deciSeconds2 >= checkcoins * 3)
+    if (deciSeconds2 >= checkcoins * 1.2)
     {
       CurrencyChecker(1, 2, 1, "coin");
       CurrencyChecker(3, 6, 5, "coin");
@@ -197,9 +271,13 @@ void RunCodeInMillis()
     deciSeconds1++;
     deciSeconds2++;
     Keypad_control();
-    control_function();
-    LCD_display_customer();
-
+    if (userMode == "ADMIN_mode")
+    { 
+      control_function_admin();
+    }else{
+      control_function_customer();
+      LCD_display();
+    }
   }
 }
 
